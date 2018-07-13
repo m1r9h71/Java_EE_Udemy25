2ac994f3d1ce60e8b25ec1f4cc369bca6261d2a2
m1r9h71 <matthoing@icloud.com>
2018-07-13 15:54:03 +0100

    Nature of statless sessions in EJB

--- src/com/airline/controllers/FlightDetails.java
+++ src/com/airline/controllers/FlightDetails.java
@@ -18,35 +18,69 @@ import com.airline.service.FlightService;
 @WebServlet("/FlightDetails")
 public class FlightDetails extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-	
+
 	@EJB
 	private FlightService fs;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public FlightDetails() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
+
+	@EJB
+	private FlightService fs2;
+
+	@EJB
+	private FlightService fs3;
+
+	@EJB
+	private FlightService fs4;
+
+	@EJB
+	private FlightService fs5;
+
+	@EJB
+	private FlightService fs6;
 
 	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#HttpServlet()
 	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		//response.getWriter().append("Served at: ").append(request.getContextPath());
-		
-		PrintWriter out = response.getWriter();
-		out.println("The flights details servlet has been called.....");
-		out.println(fs.getAirplaneModel());
-		out.println(fs.getFrom());
+	public FlightDetails() {
+		super();
+		// TODO Auto-generated constructor stub
 	}
 
 	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
 	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		// response.getWriter().append("Served at: ").append(request.getContextPath());
+
+		PrintWriter out = response.getWriter();
+		out.println("The flights details servlet has been called.....");
+		fs.setFrom("London");
+		out.println(fs.getFrom());
+		fs2.setFrom("Rome");
+		out.println(fs.getFrom());
+
+		fs3.setFrom("New York");
+		out.println(fs.getFrom());
+
+		fs4.setFrom("Paris");
+		out.println(fs.getFrom());
+
+		fs5.setFrom("San Francisco");
+		out.println(fs.getFrom());
+
+		fs6.setFrom("Waterford");
+		out.println(fs.getFrom());
+
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		doGet(request, response);
 	}
